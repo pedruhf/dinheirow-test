3163798f9c4a9176615b9b2cc4e35e0b40756b5f
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-11-13 19:59:36 -0300

    feat: adds details route

--- src/main/routes/index.tsx
+++ src/main/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { makeHomeComponent } from "@/main/factories/presentation/views";
+import { makeHomeComponent, makeCharacterDetailsComponent } from "@/main/factories/presentation/views";
 
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={makeHomeComponent()} />
+        <Route path="/characters/details/:id" element={makeCharacterDetailsComponent()} />
         <Route path="*" element={makeHomeComponent()} />
       </Routes>
     </BrowserRouter>
---
+++ src/presentation/views/character-details/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss"
+
+export const CharacterDetails: React.FC = () => {
+  return (
+    <div className={styles.characterDetailsWrapper}>
+      teste
+    </div>
+  );
+}
