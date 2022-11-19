4e8147199d0fdb176f650fdbe39a066d227c4715
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-11-19 20:26:58 -0300

    refactor: remove unnecessary code

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const root = join(__dirname, "..", "dist");
-console.log('directory-name 👉️', root);
+
 app.use(express.static(root));
 app.use(fallback("index.html", { root }));
 app.listen(3000, () => console.log("SERVER RUNNING AT PORT 3000"));
